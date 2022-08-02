2eeabf2f1a569fc20cd44bb868c2575bc63c42e1
YEGU KWON <wt2080@gmail.com>
2022-08-02 21:02:10 +0900

    Update PixelToCoordinateTranslator.cs

--- PixelToCoordinate/PixelToCoordinateTranslator.cs
+++ PixelToCoordinate/PixelToCoordinateTranslator.cs
@@ -7,10 +7,10 @@ namespace SatGS.PathFinder
     public class PixelToCoordinateTranslator
     {
         private readonly int[] rowDegreeList = { -65, -60, -55, -50, -45, -40, -35, -30, -25, -20, -15, -10, -5, 0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65 };
-        private readonly int[] colDegreeList = { -50, -45, -40, -35, -30, -25, -20, -15, -10, -5, 0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50};
+        private readonly int[] colDegreeList = { 50, 45, 40, 35, 30, 25, 20, 15, 10, 5, 0, -5, -10, -15, -20, -25, -30, -35, -40, -45, -50 };
 
         private readonly int[] rowDiffRadial = { 25, 26, 31, 20, 25, 21, 24, 21, 25, 19, 30, 26, 27, 27, 26, 30, 19, 25, 21, 24, 21, 25, 20, 31, 26, 25 };
-                                              
+
         private List<double> rowTargetDiffRadial = new List<double>();
         private List<double> rowAccRadial = new List<double>();
 
@@ -33,66 +33,50 @@ namespace SatGS.PathFinder
 
         private void calcDistanceBetweenRadial()
         {
-            int acc = 0;
+            double acc = 0;
 
-            foreach(int diff in rowDiffRadial)
+            foreach (int diff in rowDiffRadial)
             {
-                acc += diff;
+                acc += diff * d;
                 rowAccRadial.Add(acc);
                 rowTargetDiffRadial.Add(diff * d);
             }
 
-
-            /*
-            foreach(int diff in rowDiffRadial)
-            {
-                int last = rowTargetDiffRadial.IndexOf(rowTargetDiffRadial.Count - 1);
-                int value = diff * d;
-                rowTargetDiffRadial.Add(value);
-                rowAccRadial.Add(value + last);
-            }*/
-
             acc = 0;
             foreach (int diff in colDiffRadial)
             {
-                acc += diff;
+                acc += diff * d;
                 colAccRadial.Add(acc);
                 colTargetDiffRadial.Add(diff * d);
             }
 
-            /*
-            foreach (int diff in colDiffRadial)
-            {
-                int last = colTargetDiffRadial.IndexOf(colTargetDiffRadial.Count - 1);
-                int value = diff * d;
-                colTargetDiffRadial.Add(value);
-                colAccRadial.Add(value + last);
-            }
-            */
         }
 
         private void calcDegreeByRadial()
         {
-            rowAccRadial.Reverse();
-            for(int i=0; i < rowAccRadial.Count; i++)
+            Console.WriteLine($"d: {d}");
+            for (int i = 0; i < rowAccRadial.Count; i++)
             {
-                if(rowAccRadial[i] < tx)
-                {
-                    int lessDeg = rowDegreeList[i];
-                    phi = (tx - rowAccRadial[i]) / rowTargetDiffRadial[i] * 5 + lessDeg;
-                    Console.WriteLine($"tx: {tx}, rowAccRadial[i] = {rowAccRadial[i]}, phi = {tx - rowAccRadial[i]}/{rowTargetDiffRadial[i] * 5} + {lessDeg} = {phi}");
+                if (rowAccRadial[i+1] > tx) {
+                    int offset = tx < 320 ? -5 : 5;
+                    double lessDeg = (double)rowDegreeList[i];
+                    phi = (((double)(tx - rowAccRadial[i]) / (double)rowTargetDiffRadial[i]) * (double)(offset)) + (double)lessDeg;
+                    Console.WriteLine($"{(double)(tx - rowAccRadial[i])}");
+                    Console.WriteLine($"{((double)(tx - rowAccRadial[i]) / (double)rowTargetDiffRadial[i])}");
+                    Console.WriteLine($"{(((double)(tx - rowAccRadial[i]) / (double)rowTargetDiffRadial[i]) * (double)(offset))}");
+                    Console.WriteLine($"tx: {tx}, i:{i}, rowAccRadial[i] = {rowAccRadial[i]}, phi = {tx - rowAccRadial[i]}/{rowTargetDiffRadial[i] * offset} + {lessDeg} = {phi}");
                     break;
                 }
             }
 
-            colAccRadial.Reverse();
             for (int i = 0; i < colAccRadial.Count; i++)
             {
-                if (colAccRadial[i] < tx)
+                if (colAccRadial[i+1] > ty)
                 {
+                    int offset = tx < 240 ? 5 : -5;
                     int lessDeg = colDegreeList[i];
-                    theta = 90 - ((ty - colAccRadial[i]) / colTargetDiffRadial[i] * 5 + lessDeg);
-                    Console.WriteLine($"ty = {ty}, colAccRadial[i] = {colAccRadial[i]}, theta = {ty - colAccRadial[i]}/{colTargetDiffRadial[i] * 5} + {lessDeg} = {theta}");
+                    theta = (double)90 - ((double)(ty - colAccRadial[i]) / (double)colTargetDiffRadial[i] * (double)offset + (double)lessDeg);
+                    Console.WriteLine($"ty = {ty}, colAccRadial[i] = {colAccRadial[i]}, theta = 90 - {ty - colAccRadial[i]}/{colTargetDiffRadial[i] * offset} + {lessDeg} = {theta}");
                     break;
                 }
             }
