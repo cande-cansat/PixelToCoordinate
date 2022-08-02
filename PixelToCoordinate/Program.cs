9bae8a06eed29ad9e58339cb3e37b820560049d0
BrawnyClover <sonbill971@gmail.com>
2022-08-03 03:11:19 +0900

    Generate system reviser

--- PixelToCoordinate/Program.cs
+++ PixelToCoordinate/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace PixelToCoordinate
 {
@@ -6,9 +7,26 @@ namespace PixelToCoordinate
     {
         static void Main(string[] args)
         {
-            int px, py, d;
-            PixelToCoordinateTranslator pTranslator = new PixelToCoordinateTranslator(px, py, d);
+            double px, py, d;
+            float roll, pitch, yaw;
+            px = 248;
+            py = 195;
+            d = 0.109486835375;
 
+            roll = 0;
+            pitch = 0;
+            yaw = 0;
+
+            PixelToCoordinateTranslator pTranslator = new PixelToCoordinateTranslator(px, py, d);
+            List<double> coordinateValue = pTranslator.calcDegreeFromPixel();
+            Coordinate spherical = new Coordinate((float)coordinateValue[0], (float)coordinateValue[1], (float)coordinateValue[2]);
+            Coordinate cartesian = Coordinate.sphericalToCartesian(spherical);
+
+            SystemReviser reviser = new SystemReviser(roll, pitch, yaw);
+            Coordinate targetCoordinateCartessian = reviser.revise(cartesian);
+
+
+            Console.WriteLine();
         }
     }
 }
---
+++ PixelToCoordinate/SystemReviser.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PixelToCoordinate
+{
+    class SystemReviser
+    {
+        private float roll, pitch, yaw;
+        private Coordinate target;
+        private float[,] reviseMatrix;
+        public SystemReviser()
+        {
+
+        }
+        public SystemReviser(float roll, float pitch, float yaw)
+        {
+            this.roll = roll;
+            this.pitch = pitch;
+            this.yaw = yaw;
+        }
+
+        private float cos(float v)
+        {
+            return (float)Math.Cos(v);
+        }
+        private float sin(float v)
+        {
+            return (float)Math.Sin(v);
+        }
+        public Coordinate revise(Coordinate cartesian, float roll, float pitch, float yaw)
+        {
+            reviseMatrix = new float[3,3]
+            {
+                {
+                    cos(pitch)*cos(roll),
+                    sin(yaw)*sin(pitch)*cos(roll) - cos(yaw)*sin(roll),
+                    cos(yaw)*sin(pitch)*cos(roll)+sin(yaw)*sin(roll)
+                },
+                {
+                    cos(pitch)*sin(roll),
+                    sin(yaw)*sin(pitch)*sin(roll) + cos(yaw)*cos(roll),
+                    cos(yaw)*sin(pitch)*sin(roll)-sin(yaw)*cos(roll)
+                },
+                {
+                    -sin(pitch), sin(yaw)*cos(pitch), cos(yaw)*cos(pitch)
+                }
+            };
+            List<float> revisedCoordinate = new List<float>();
+
+            for(int i=0; i<3; i++)
+            {
+                revisedCoordinate.Add(reviseMatrix[i,0] * cartesian.item1 + reviseMatrix[i,1] * cartesian.item2 + reviseMatrix[i,2] * cartesian.item3);
+            }
+            Coordinate retVal = new Coordinate(revisedCoordinate[0], revisedCoordinate[1], revisedCoordinate[2]);
+            return retVal;
+        }
+
+    }
+}
